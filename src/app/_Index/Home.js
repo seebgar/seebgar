93533ab2be8e8107560d06cc907475bf95e6c8a3
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2019-08-20 09:19:41 -0500

    edge

--- src/app/_Index/Home.js
+++ src/app/_Index/Home.js
@@ -7,9 +7,11 @@ import "./index.scss";
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    const isFirefox = typeof InstallTrigger !== 'undefined'; 
+    const isFirefox = typeof InstallTrigger !== "undefined";
+    const isIE = false || !!document.documentMode;
+    const isEdge = !isIE && !!window.StyleMedia;
 
-    this.state = { scroll: 0, isFirefox };
+    this.state = { scroll: 0, isFirefox, isEdge, isIE };
   }
 
   componentDidMount() {
@@ -148,41 +150,45 @@ class Home extends React.Component {
               style={{ fontSize: "36px", color: "rgba(50, 50, 50,0)" }}
               name="arrow-down"
             />
-            {!this.state.isFirefox ?
+            {!this.state.isFirefox || this.state.isEdge || this.state.isIE ? (
               <svg
-              version="1.1"
-              id="Layer_1"
-              xmlns="http://www.w3.org/2000/svg"
-              xlink="http://www.w3.org/1999/xlink"
-              x="0px"
-              y="0px"
-              viewBox="0 0 210.4 210.4"
-              space="preserve"
-            >
-              <g>
-                <path
-                  className="st0"
-                  d="M105.2,0C66.5,0,35.1,31.5,35.1,70.1v70.2c0,38.7,31.5,70.1,70.1,70.1s70.1-31.4,70.1-70.1V70.1
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 210.4 210.4"
+                space="preserve"
+              >
+                <g>
+                  <path
+                    className="st0"
+                    d="M105.2,0C66.5,0,35.1,31.5,35.1,70.1v70.2c0,38.7,31.5,70.1,70.1,70.1s70.1-31.4,70.1-70.1V70.1
 		C175.3,31.5,143.9,0,105.2,0z M167.6,140.3c0,34.4-28,62.3-62.3,62.3S43,174.7,43,140.3V70.1c0-34.4,28-62.3,62.3-62.3
 		s62.3,28,62.3,62.3V140.3z"
-                />
-              </g>
-              <g>
+                  />
+                </g>
                 <g>
                   <g>
-                    <path
-                      className="st0" id="st1"
-                      d="M105.2,23.4c-2.2,0-3.9,1.4-3.9,3.2v28.9c0,1.8,1.7,3.2,3.9,3.2s3.9-1.4,3.9-3.2V26.6C109.1,24.8,107.4,23.4,105.2,23.4z
+                    <g>
+                      <path
+                        className="st0"
+                        id="st1"
+                        d="M105.2,23.4c-2.2,0-3.9,1.4-3.9,3.2v28.9c0,1.8,1.7,3.2,3.9,3.2s3.9-1.4,3.9-3.2V26.6C109.1,24.8,107.4,23.4,105.2,23.4z
 				"
-                    />
+                      />
+                    </g>
                   </g>
                 </g>
-              </g>
-            </svg> :  <ion-icon
-              style={{ fontSize: "36px", color: "white" }}
-              name="arrow-down" id="ArrowAnim"
-            />}
-            
+              </svg>
+            ) : (
+              <ion-icon
+                style={{ fontSize: "36px", color: "white" }}
+                name="arrow-down"
+                id="ArrowAnim"
+              />
+            )}
           </div>
         </div>
 
